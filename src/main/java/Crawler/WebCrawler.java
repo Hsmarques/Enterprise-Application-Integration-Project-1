1eb16f6fb656ee9a8441941d237b1ca24b47dbe7
Hsmarques <mrhsmar@gmail.com>
2015-10-13 23:26:11 +0100

    Added method

--- src/main/java/Crawler/WebCrawler.java
+++ src/main/java/Crawler/WebCrawler.java
@@ -13,12 +13,6 @@ import org.jsoup.select.Elements;
 import generated.Smartphone;
 
 public class WebCrawler {
-<<<<<<< HEAD
-
-=======
-	
-	
->>>>>>> refs/remotes/origin/master
 	public static void main(String[] args) {
 
 		Properties prop = new Properties();
@@ -29,101 +23,22 @@ public class WebCrawler {
 			prop.load(input);
 		} catch (IOException ex) {
 			ex.printStackTrace();
-		} finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 
 		Document dom = null;
 		Document dom_child = null;
 		
-<<<<<<< HEAD
 		dom = jSoupLoader(prop.getProperty("url"));
-
-		Elements phoneList = dom.select("header.productTitle");
-
-		for (Element e : phoneList) {
-
-			Smartphone smartphone = new Smartphone();
-
-			dom_child = jSoupLoader(e.child(0).absUrl("href"));
-
-			Elements titlePicker = dom_child.select(
-					".pageTitle > span");/*
-											 * Gets the title tag and content
-											 */
-			Elements pricePicker = dom_child.select("div.currentPrice").select("ins");
-			Elements categoriesPicker = dom_child.select(".simpleTable tr");
-
-			for (Element phoneDesc : categoriesPicker) {
-
-				switch (phoneDesc.select("td").text().toLowerCase()) {
-
-				case "sistema operativo":
-					System.out.println(categoriesPicker.select("td").text());
-					break;
-				case "processador":
-					System.out.println(categoriesPicker.select("td").text());
-					break;
-				default:
-					System.out.println("default break");
-					break;
-
-				}
-			}
-			Elements infoPicker = dom_child
-					.select(".simpleTable td");/*
-												 * Gets the phone specifications
-												 * tag and content
-												 */
-
-			/*
-			 * System.out.println("\n" +"Model:" + titlePicker.text() + "\n");
-			 * System.out.println("Price:" + pricePicker.text() + "\n");
-			 * System.out.println(categoriesPicker.text());
-			 */
-
-		}
 
 		String title = dom.title();
 		System.out.println(title);
-	}
-
-	public static Document jSoupLoader(String url) {
-		Document dom = null;
-		try {
-			// Load the DOM
-			dom = Jsoup.connect(url).timeout(0).get();
-=======
-		Document dom = null;
-		Document dom_child = null;
-		
-		try {
-			
-			//Load the DOM
-			dom = Jsoup.connect(prop.getProperty("url")).timeout(0).get();
-		
->>>>>>> refs/remotes/origin/master
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-<<<<<<< HEAD
-		return dom;
-=======
 		
 		Elements phoneList = dom.select("header.productTitle");
 		
 		for(Element e: phoneList){
 			//System.out.println("Next URL: "+ e.child(0).absUrl("href"));
-			try {
 				
-				dom_child = Jsoup.connect(e.child(0).absUrl("href")).timeout(0).get();
+				dom_child = jSoupLoader(e.child(0).absUrl("href"));
 				
 				Elements titlePicker = dom_child.select(".pageTitle > span");/*Gets the title tag and content*/
 				Elements pricePicker = dom_child.select("div.currentPrice").select("ins");
@@ -180,17 +95,18 @@ public class WebCrawler {
 						break;
 				
 					}
-				}
-		
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
+				}	
 		}
-		
-		String title = dom.title();
-		System.out.println(title);
->>>>>>> refs/remotes/origin/master
+	}
+	public static Document jSoupLoader(String url) {
+		Document dom = null;
+		try {
+			// Load the DOM
+			dom = Jsoup.connect(url).timeout(0).get();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dom;
 	}
 }
