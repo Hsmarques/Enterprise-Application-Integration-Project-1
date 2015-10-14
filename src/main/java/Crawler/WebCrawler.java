556cf8f95fc5fb5a51fa8ddb8bcb2d1e7bef1c7a
Hsmarques <mrhsmar@gmail.com>
2015-10-14 14:35:07 +0100

    added marshall

--- src/main/java/Crawler/WebCrawler.java
+++ src/main/java/Crawler/WebCrawler.java
@@ -1,18 +1,30 @@
 package Crawler;
 
+import generated.Screen;
+import generated.Smartphone;
+import generated.Smartphones;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import generated.Smartphone;
-
 public class WebCrawler {
+	
+	public List smartphoneList =new ArrayList();
+    
 	public static void main(String[] args) {
 
 		Properties prop = new Properties();
@@ -23,80 +35,122 @@ public class WebCrawler {
 			prop.load(input);
 		} catch (IOException ex) {
 			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 
 		Document dom = null;
 		Document dom_child = null;
-		
+
+		// Load the DOM
 		dom = jSoupLoader(prop.getProperty("url"));
 
-		String title = dom.title();
-		System.out.println(title);
-		
 		Elements phoneList = dom.select("header.productTitle");
-		
-		for(Element e: phoneList){
-			//System.out.println("Next URL: "+ e.child(0).absUrl("href"));
-				
-				dom_child = jSoupLoader(e.child(0).absUrl("href"));
-				
-				Elements titlePicker = dom_child.select(".pageTitle > span");/*Gets the title tag and content*/
-				Elements pricePicker = dom_child.select("div.currentPrice").select("ins");
-				Elements categoriesPicker = dom_child.select(".simpleTable tr");
-				
-			
-				
-				
-				for(Element aspects : categoriesPicker){
-					
-					
-					
-					switch(aspects.select("th").text().toLowerCase()){
-				
-						case "sistema operativo":
-							System.out.println("so: " + aspects.select("td").text());
+
+		for (Element e : phoneList) {
+			// System.out.println("Next URL: "+ e.child(0).absUrl("href"));
+			dom_child = jSoupLoader(e.child(0).absUrl("href"));
+
+			Smartphones smartphones = new Smartphones();
+			Smartphone smartphone = new Smartphone();
+			Screen screen = new Screen();
+
+			Elements titlePicker = dom_child.select(".pageTitle > span");
+			Elements pricePicker = dom_child.select("div.currentPrice").select("ins");
+			Elements categoriesPicker = dom_child.select("table.simpleTable tr");
+
+			System.out.println("=======================================");
+			System.out.println("Model:" + titlePicker.text());
+			System.out.println("Preco" + pricePicker.text());
+
+			for (Element aspects : categoriesPicker) {
+
+				switch (aspects.select("th").text().toLowerCase()) {
+
+				case "sistema operativo":
+					smartphone.setSo(aspects.select("td").text());
+					System.out.println("so: " + aspects.select("td").text());
+					break;
+				case "processador":
+					smartphone.setProcessor(aspects.select("td").text());
+					System.out.println("processador:" + aspects.select("td").text());
+					break;
+				case "tecnologia do ecrã":
+					screen.setType(aspects.select("td").text());
+					System.out.println("tecnologia ecra: " + aspects.select("td").text());
+					break;
+				case "tamanho do ecrã":
+					screen.setType(aspects.select("td").text());
+					System.out.println("tamanho ecra:" + aspects.select("td").text());
+					break;
+				case "frequências":
+					System.out.println("Frequências:" + aspects.select("td").text());
+					break;
+				case "redes":
+					System.out.println("redes:" + aspects.select("td").text());
+					break;
+				case "bluetooth":
+					System.out.println("bluetooth:" + aspects.select("td").text());
+					break;
+				case "wi-fi":
+					System.out.println("wi-fi:" + aspects.select("td").text());
+					break;
+				case "resolução máxima (em pixeis)":
+
+					if (aspects.parent().previousElementSibling().text().toLowerCase().equals("máquina fotográfica")) {
+						System.out.println("câmara:" + aspects.select("td").text());
 						break;
-						case "tecnologia do ecrã":  
-							System.out.println("tecnologia ecra: "+ aspects.select("td").text()+"\n");
+					} else {
 						break;
-						case "tamanho do ecrã":  
-							System.out.println("tamanho ecra:" + aspects.select("td").text());
-						break;
-						case "frequências":  
-							System.out.println("Frequências:" + aspects.select("td").text());
-						break;
-						case "redes":  
-							System.out.println("redes:" + aspects.select("td").text());
-						break;
-						case "bluetooth":  
-							System.out.println("bluetooth:" + aspects.select("td").text());
-						break;
-						case "wifi":  
-							System.out.println("wifi:" + aspects.select("td").text());
-						break;
-						case "câmera frontal":
-							System.out.println("CAMERAAAAAA");
-							if(aspects.parent().previousElementSibling().text().toLowerCase() == "fotografia")
-								System.out.println("funciona!!!!!!!!!!!!!");
-							else
-								System.out.println("nao funciona ");
-						break;
-						case "câmera traseira":
-						break;	
-						case "bateria":
-						break;
-						case "autonomia":
-						break;
-						case "dimensoes":
-						break;
-						case "pesos":
-						break;
-						default://System.out.println("default break");
-						break;
-				
 					}
-				}	
+				case "câmera frontal":
+
+					break;
+
+				case "bateria":
+					System.out.println(aspects.select("td").text());
+					break;
+				case "autonomia":
+					System.out.println(aspects.select("td").text());
+					break;
+				case "dimensoes":
+					System.out.println(aspects.select("td").text());
+					break;
+				case "peso":
+					System.out.println(aspects.select("td").text());
+					break;
+				default:
+					break;
+
+				}
+			}
+
 		}
+	}
+
+	public static void marshall(Smartphones smartphones){ 
+	
+		try {
+			StringWriter stringwriter = new StringWriter();
+			
+			JAXBContext jaxbContext = JAXBContext.newInstance(Smartphones.class);
+			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+			// output pretty printed
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			jaxbMarshaller.marshal(smartphoneList, stringwriter);
+
+		      } catch (JAXBException e) {
+			e.printStackTrace();
+		      }
+	 
 	}
 	public static Document jSoupLoader(String url) {
 		Document dom = null;
